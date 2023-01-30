cb0f01806373c5bcc83ff302787748a698e0ee36
Taoning Ge <gtn1024o@outlook.com>
2023-01-30 19:34:10 +0800

    按需引入element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
 import App from './App.vue'
@@ -21,7 +20,6 @@ router.afterEach((to) => {
   }
 })
 
-app.use(ElementPlus)
 app.use(createPinia())
 app.use(router)
 
--- vite.config.base.ts
+++ vite.config.base.ts
@@ -3,6 +3,9 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +14,12 @@ export default defineConfig({
     vueJsx({
       transformOn: true,
       mergeProps: true,
+    }),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
     }),
   ],
   resolve: {
