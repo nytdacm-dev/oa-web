1adafbd838297a2926bab3c468d1d376517761a5
Taoning Ge <gtn1024o@outlook.com>
2023-02-02 13:41:30 +0800

    重构默认布局

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,25 +1,25 @@
-import { ElContainer, ElFooter, ElHeader, ElMain } from "element-plus";
 import { defineComponent } from "vue";
 import { RouterView } from "vue-router";
 import Header from "./default-layout/Header.vue";
 import Footer from "./default-layout/Footer.vue";
 import style from "./DefaultLayout.module.scss"
+import { NLayout, NLayoutContent, NLayoutFooter, NLayoutHeader } from "naive-ui";
 
 export const DefaultLayout = defineComponent({
   setup() {
     return () => (
       <>
-        <ElContainer>
-          <ElHeader>
+        <NLayout contentStyle={ "overflow-x: unset; " }>
+          <NLayoutHeader>
             <Header />
-          </ElHeader>
-          <ElMain class={style.main}>
+          </NLayoutHeader>
+          <NLayoutContent contentStyle={ "padding-left:24px;" } class={ style.main }>
             <RouterView />
-          </ElMain>
-          <ElFooter class={style.footer}>
+          </NLayoutContent>
+          <NLayoutFooter class={ style.footer }>
             <Footer />
-          </ElFooter>
-        </ElContainer>
+          </NLayoutFooter>
+        </NLayout>
       </>
     )
   }
