e39e40482fcd920c7936fa965cabd31330fdf0be
Taoning Ge <gtn1024o@outlook.com>
2023-02-02 23:31:14 +0800

    Revert "使用 Gradle 构建前端"
    This reverts commit 54c8628a56147b2e9ed4f09214cbea293d22eb27.

--- vite.config.base.ts
+++ vite.config.base.ts
@@ -27,4 +27,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  build: {
+    outDir: '../src/main/resources/static/',  // 构建至 Spring Boot static 目录
+  }
 })
