147af31f29af8c16aaaa1d5e7e2b673a44e7d24d
Taoning Ge <gtn1024o@outlook.com>
2023-02-23 10:37:42 +0800

    修复编译失败的问题

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,7 +10,7 @@ import Link from "@/components/Link.vue";
       <ul>
         <li>
           新手入门题单：
-          <Link href="https://ac.nowcoder.com/discuss/817596" new-window="true">牛客</Link>
+          <Link href="https://ac.nowcoder.com/discuss/817596" :new-window="true">牛客</Link>
         </li>
         <li>
           <span>
@@ -18,7 +18,7 @@ import Link from "@/components/Link.vue";
             <Link href="https://book.douban.com/subject/36132750/" :new-window="true">算法竞赛</Link>
             》题单：
           </span>
-          <Link href="https://vjudge.net/article/3284" new-window="true">Vjudge Workbook</Link>
+          <Link href="https://vjudge.net/article/3284" :new-window="true">Vjudge Workbook</Link>
         </li>
         <li>
           <span>
@@ -26,11 +26,11 @@ import Link from "@/components/Link.vue";
             <Link href="https://book.douban.com/subject/24749842/" :new-window="true">挑战程序设计竞赛</Link>
             》题单：
           </span>
-          <Link href="https://vjudge.net/article/46" new-window="true">Vjudge Workbook</Link>
+          <Link href="https://vjudge.net/article/46" :new-window="true">Vjudge Workbook</Link>
         </li>
         <li>
           <span>kuangbin带你飞系列题单：</span>
-          <Link href="https://vjudge.net/article/187" new-window="true">Vjudge Workbook</Link>
+          <Link href="https://vjudge.net/article/187" :new-window="true">Vjudge Workbook</Link>
         </li>
         <li>
           <span>
@@ -38,14 +38,14 @@ import Link from "@/components/Link.vue";
             <Link href="https://book.douban.com/subject/30136932/" :new-window="true">算法竞赛进阶指南</Link>
             》题单：
           </span>
-          <Link href="https://ac.nowcoder.com/acm/archive/oi-advance" new-window="true">
+          <Link href="https://ac.nowcoder.com/acm/archive/oi-advance" :new-window="true">
             牛客（推荐，可以计算提交记录）
           </Link>
-          <Link href="https://www.acwing.com/activity/content/6/" new-window="true">Acwing</Link>
+          <Link href="https://www.acwing.com/activity/content/6/" :new-window="true">Acwing</Link>
         </li>
         <li>
           蓝桥杯题单：
-          <Link href="https://vjudge.net/article/3458" new-window="true">Vjudge Workbook</Link>
+          <Link href="https://vjudge.net/article/3458" :new-window="true">Vjudge Workbook</Link>
         </li>
       </ul>
     </div>
