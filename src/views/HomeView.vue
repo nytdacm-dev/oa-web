93a9e0b2c36186cc48708d607f747f4513234625
Taoning Ge <gtn1024o@outlook.com>
2023-03-08 15:53:13 +0800

    修改首页

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,7 +17,10 @@ import Link from '@/components/Link.vue'
         <li>
           <span>
             《
-            <Link href="https://union-click.jd.com/jdc?e=618%7Cpc%7C&p=JF8BANgJK1olXwUFU1xcAUoRA18IGV4QXwACUG4ZVxNJXF9RXh5UHw0cSgYYXBcIWDoXSQVJQwYAUVtfDksTHDZNRwYlJm8EDh8GdjV1aDtwXh4QNlJ0PSUPXkcbM244G1oUXgMAUlpUD3snA2g4STXN67Da8e9B3OGY1uefK1olXQEEUlpcAUseAWgJHWsSXQ8yDwszfiBUQBRNTyRLO1tiZG5tC3sQA2YcHSlUDxIEJm5tCHsUM28AGFsUVQIEVEJdCUkTCnMIHF0TWQcLVFhVCk8eM20JGl8cbTY" :new-window="true">算法竞赛</Link>
+            <Link
+              href="https://union-click.jd.com/jdc?e=618%7Cpc%7C&p=JF8BANgJK1olXwUFU1xcAUoRA18IGV4QXwACUG4ZVxNJXF9RXh5UHw0cSgYYXBcIWDoXSQVJQwYAUVtfDksTHDZNRwYlJm8EDh8GdjV1aDtwXh4QNlJ0PSUPXkcbM244G1oUXgMAUlpUD3snA2g4STXN67Da8e9B3OGY1uefK1olXQEEUlpcAUseAWgJHWsSXQ8yDwszfiBUQBRNTyRLO1tiZG5tC3sQA2YcHSlUDxIEJm5tCHsUM28AGFsUVQIEVEJdCUkTCnMIHF0TWQcLVFhVCk8eM20JGl8cbTY"
+              :new-window="true"
+            >算法竞赛</Link>
             》题单：
           </span>
           <Link href="https://vjudge.net/article/3284" :new-window="true">
@@ -27,7 +30,10 @@ import Link from '@/components/Link.vue'
         <li>
           <span>
             《
-            <Link href="https://union-click.jd.com/jdc?e=618%7Cpc%7C&p=JF8BANgJK1olXwUFU1xcAUoRA18IG1gSWgEHXW4ZVxNJXF9RXh5UHw0cSgYYXBcIWDoXSQVJQwYCV1laD04eHDZNRwYlLVVXJyBfbBN1QGgWWzJ8OFVqKVsleEcbM244G1oUXgMAUlpUD3snA2g4STXN67Da8e9B3OGY1uefK1olXQEEUlpcAUgXBW0KG2sSXQ8yDwszfiBUQBRNTyRLO1tiZG5tC3sQA2YcHSlUDxIEJm5tCHsUM28AGVoXXgUCV0JdCU4UAXMIHF0TWQcLV19VCk0XM20JGl8cbTY" :new-window="true">挑战程序设计竞赛</Link>
+            <Link
+              href="https://union-click.jd.com/jdc?e=618%7Cpc%7C&p=JF8BANgJK1olXwUFU1xcAUoRA18IG1gSWgEHXW4ZVxNJXF9RXh5UHw0cSgYYXBcIWDoXSQVJQwYCV1laD04eHDZNRwYlLVVXJyBfbBN1QGgWWzJ8OFVqKVsleEcbM244G1oUXgMAUlpUD3snA2g4STXN67Da8e9B3OGY1uefK1olXQEEUlpcAUgXBW0KG2sSXQ8yDwszfiBUQBRNTyRLO1tiZG5tC3sQA2YcHSlUDxIEJm5tCHsUM28AGVoXXgUCV0JdCU4UAXMIHF0TWQcLV19VCk0XM20JGl8cbTY"
+              :new-window="true"
+            >挑战程序设计竞赛</Link>
             》题单：
           </span>
           <Link href="https://vjudge.net/article/46" :new-window="true">
@@ -43,7 +49,10 @@ import Link from '@/components/Link.vue'
         <li>
           <span>
             《
-            <Link href="https://union-click.jd.com/jdc?e=618%7Cpc%7C&p=JF8BAN0JK1olXgcCU1lVCk8SC18IGloSWwUAXFhaCEsSBF9MRANLAjZbERscSkAJHTdNTwcKBlMdBgABFksWAmgOGFkdWwECVFtaFxJSXzI4RTBhFQFmNF49aylkQDINGVt1GnFaJFJROEonA24JGF4XWwILU25tCEwnQgEIH1gWXwcyVW5dD00RB24BG1sTXAQDZFldAXtMVgF-cBhWJkNWKwA7VSsnM18LK1wVVBIEJh8PHE1lM18IK1glXQ4AVVxdCUkeH28JGlsTQQYFUlhZCUIXA24JH1MlXwcDUFdtOA" :new-window="true">算法竞赛进阶指南</Link>
+            <Link
+              href="https://union-click.jd.com/jdc?e=618%7Cpc%7C&p=JF8BAN0JK1olXgcCU1lVCk8SC18IGloSWwUAXFhaCEsSBF9MRANLAjZbERscSkAJHTdNTwcKBlMdBgABFksWAmgOGFkdWwECVFtaFxJSXzI4RTBhFQFmNF49aylkQDINGVt1GnFaJFJROEonA24JGF4XWwILU25tCEwnQgEIH1gWXwcyVW5dD00RB24BG1sTXAQDZFldAXtMVgF-cBhWJkNWKwA7VSsnM18LK1wVVBIEJh8PHE1lM18IK1glXQ4AVVxdCUkeH28JGlsTQQYFUlhZCUIXA24JH1MlXwcDUFdtOA"
+              :new-window="true"
+            >算法竞赛进阶指南</Link>
             》题单：
           </span>
           <Link href="https://ac.nowcoder.com/acm/archive/oi-advance" :new-window="true">
@@ -58,6 +67,30 @@ import Link from '@/components/Link.vue'
           <Link href="https://vjudge.net/article/3458" :new-window="true">
             Vjudge Workbook
           </Link>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <NH2>学点课吧</NH2>
+      <ul>
+        <li>
+          <Link
+            href="https://www.nowcoder.com/courses/cover/live/724?coupon=A6t6JmF"
+            :new-window="true"
+          >
+            牛客竞赛算法入门班
+          </Link>
+        </li>
+        <li>
+          <span>
+            <Link
+              href="https://www.acwing.com/user/security/school_verify/ac_stars/"
+              :new-window="true"
+            >
+              Acwing 认证学生身份领 AC 币
+            </Link>
+            邀请码：WYHHY
+          </span>
         </li>
       </ul>
     </div>
