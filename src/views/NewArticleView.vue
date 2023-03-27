49375f93114402c576d74febaedcec0ddcf9fd3c
Taoning Ge <gtn1024o@outlook.com>
2023-03-27 07:38:45 +0000

    添加提示

--- src/views/NewArticleView.vue
+++ src/views/NewArticleView.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { NButton, NInput, NSpace } from 'naive-ui'
+import { NButton, NInput, NSpace, useNotification } from 'naive-ui'
 import { useRouter } from 'vue-router'
+import type { AxiosError } from 'axios'
 import MDEditor from '@/components/MDEditor.vue'
+import type { HttpResponse } from '@/shared/Http'
 import { http } from '@/shared/Http'
 import type { Models } from '@/models/models'
 
+const notification = useNotification()
 const router = useRouter()
 const titleRef = ref<string>('')
 const contentRef = ref<string>('')
@@ -13,9 +16,22 @@ const publishAction = () => {
   http.post<Models.Article>('/article', {
     title: titleRef.value,
     content: contentRef.value,
-  }).then((_res) => {
-    router.push({ name: 'article-list' })
   })
+    .then((_res) => {
+      notification.success({
+        title: '发布成功',
+        content: '请等待管理员审核后发布到首页',
+        duration: 2000,
+      })
+      router.push({ name: 'article-list' })
+    })
+    .catch((err: AxiosError<HttpResponse>) => {
+      notification.error({
+        title: '发布失败',
+        content: err.response?.data.message,
+        duration: 2000,
+      })
+    })
 }
 </script>
 
