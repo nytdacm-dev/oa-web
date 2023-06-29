88220a1af725cfa92ad154a5ee9de6799c021acd
Taoning Ge <gtn1024o@outlook.com>
2023-06-29 16:34:34 +0800

    移除按需引入

--- vite.config.base.ts
+++ vite.config.base.ts
@@ -3,9 +3,6 @@ import { URL, fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,12 +11,6 @@ export default defineConfig({
     vueJsx({
       transformOn: true,
       mergeProps: true,
-    }),
-    AutoImport({
-      resolvers: [NaiveUiResolver()],
-    }),
-    Components({
-      resolvers: [NaiveUiResolver()],
     }),
   ],
   resolve: {
