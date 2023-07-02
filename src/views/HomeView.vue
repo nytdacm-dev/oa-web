21e37d997dfeb9e8964907edbad173b8345be0bc
Taoning Ge <gtn1024o@outlook.com>
2023-07-02 21:53:22 +0800

    添加题单

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,10 +3,11 @@ import MDArticle from '@/components/MDArticle.vue'
 
 const article = `## 做点题吧
 
+- [nytd acm 萌新入坑训练题单](https://vjudge.net/article/3768)
 - 新手入门题单： [牛客](https://ac.nowcoder.com/discuss/817596)
 - 《[算法竞赛](https://union-click.jd.com/jdc?e=618|pc|&p=JF8BANgJK1olXwUFU1xcAUoRA18IGV4QXwACUG4ZVxNJXF9RXh5UHw0cSgYYXBcIWDoXSQVJQwYAUVtfDksTHDZNRwYlJm8EDh8GdjV1aDtwXh4QNlJ0PSUPXkcbM244G1oUXgMAUlpUD3snA2g4STXN67Da8e9B3OGY1uefK1olXQEEUlpcAUseAWgJHWsSXQ8yDwszfiBUQBRNTyRLO1tiZG5tC3sQA2YcHSlUDxIEJm5tCHsUM28AGFsUVQIEVEJdCUkTCnMIHF0TWQcLVFhVCk8eM20JGl8cbTY)》题单： [Vjudge Workbook](https://vjudge.net/article/3284)
 - 《[挑战程序设计竞赛](https://union-click.jd.com/jdc?e=618|pc|&p=JF8BANgJK1olXwUFU1xcAUoRA18IG1gSWgEHXW4ZVxNJXF9RXh5UHw0cSgYYXBcIWDoXSQVJQwYCV1laD04eHDZNRwYlLVVXJyBfbBN1QGgWWzJ8OFVqKVsleEcbM244G1oUXgMAUlpUD3snA2g4STXN67Da8e9B3OGY1uefK1olXQEEUlpcAUgXBW0KG2sSXQ8yDwszfiBUQBRNTyRLO1tiZG5tC3sQA2YcHSlUDxIEJm5tCHsUM28AGVoXXgUCV0JdCU4UAXMIHF0TWQcLV19VCk0XM20JGl8cbTY)》题单： [Vjudge Workbook](https://vjudge.net/article/46)
-- kuangbin带你飞系列题单：[ Vjudge Workbook](https://vjudge.net/article/187)
+- kuangbin带你飞系列题单：[Vjudge Workbook](https://vjudge.net/article/187)
 - 《[算法竞赛进阶指南](https://union-click.jd.com/jdc?e=618|pc|&p=JF8BAN0JK1olXgcCU1lVCk8SC18IGloSWwUAXFhaCEsSBF9MRANLAjZbERscSkAJHTdNTwcKBlMdBgABFksWAmgOGFkdWwECVFtaFxJSXzI4RTBhFQFmNF49aylkQDINGVt1GnFaJFJROEonA24JGF4XWwILU25tCEwnQgEIH1gWXwcyVW5dD00RB24BG1sTXAQDZFldAXtMVgF-cBhWJkNWKwA7VSsnM18LK1wVVBIEJh8PHE1lM18IK1glXQ4AVVxdCUkeH28JGlsTQQYFUlhZCUIXA24JH1MlXwcDUFdtOA)》题单： [牛客（推荐，可以计算提交记录）](https://ac.nowcoder.com/acm/archive/oi-advance) [Acwing](https://www.acwing.com/activity/content/6/)
 - 蓝桥杯题单： [Vjudge Workbook](https://vjudge.net/article/3458)
 
@@ -16,9 +17,11 @@ const article = `## 做点题吧
 - [牛客竞赛算法入门班](https://www.nowcoder.com/courses/cover/live/724?coupon=A6t6JmF)
 - [Acwing 认证学生身份领 AC 币](https://www.acwing.com/user/security/school_verify/ac_stars/) 邀请码：WYHHY
 
-## 神一样的网站
+## 学习网站
 
 - [OI Wiki](https://oi-wiki.org/)
+- [牛客竞赛](https://ac.nowcoder.com)
+- [AcWing](https://www.acwing.com)
 `
 </script>
 
